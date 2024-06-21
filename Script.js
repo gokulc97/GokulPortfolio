4b02063b8296d5b15a3162f4031e7c7c5e35f003
gokulc97 <gokulcbe3@gmail.com>
2024-06-21 12:51:50 +0530

    first update

--- Script.js
+++ Script.js
@@ -1,3 +1,7 @@
 function likebutton(){
     document.getElementById("item1").innerHTML="Thanks for the Like";
 }
+
+function dislikebutton(){
+    document.getElementById("item1").innerHTML = "Please Click Like Button 👍"
+}
