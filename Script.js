20097d374414fe4fe716ae9a02f5eedea73ded6a
gokulc97 <gokulcbe3@gmail.com>
2024-07-17 08:22:42 +0530

    loader ,color-mix updated

--- Script.js
+++ Script.js
@@ -5,3 +5,11 @@ function likebutton(){
 function dislikebutton(){
     document.getElementById("item1").innerHTML = "Please Click Like Button 👍"
 }
+
+const modeToggler = document.getElementById('togglecheckbox');
+
+modeToggler.addEventListener('change', togglemode);
+
+function togglemode(){
+    document.body.classList.toggle("dark-mode");
+}
